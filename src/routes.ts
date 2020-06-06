9cc9f4483f2436d842208cf0fac58bbc09ce3b68
Diego Souza <diegosouza@bestpontos.com.br>
2020-06-06 20:06:13 -0300

    Adicionando upload de arquivo usando multer

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,38 @@
 import express from 'express';
 import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const pointsController = new PointsController();
-const itemsController = new ItemsController()
+const itemsController = new ItemsController();
 
+const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
-
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-routes.post('/points', pointsController.create);
+
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            nome: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required(),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create);
 
 
 // index - devolve uma listagem
